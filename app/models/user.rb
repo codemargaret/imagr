0414a9871ddd4893577aeb75e6b8dee14e93edb3
Dan Danilyuk and Margaret Berry <dan@>
2017-11-29 11:33:58 -0800

    add avatars to user model and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
  ## THIS IS KEY FOR LOGGING IN WITH USERNAME INSTEAD OF EMAIL:
 
  # ALSO GO TO CONFIG INITIALIZERS DEVISE.RB and UNCOMMENT LINE 37 AND CHANGE TO USERNAME
