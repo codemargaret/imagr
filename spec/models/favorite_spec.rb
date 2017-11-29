1c50088ab6dbe475ffcf323f84f5442538faf528
Dan Danilyuk and Margaret Berry <dan@>
2017-11-29 11:02:48 -0800

    add favorite and tag specs

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Favorite, 'validation' do
+  it { should validate_presence_of :image_id }
+  it { should validate_presence_of :user_id }
+end
+
+describe Favorite, 'association' do
+  it { should belong_to :user }
+  it { should belong_to :image }
+end
+
+describe Favorite, 'column_specification' do
+  it { should have_db_column(:image_id).of_type(:integer) }
+  it { should have_db_column(:user_id).of_type(:integer) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
 end
