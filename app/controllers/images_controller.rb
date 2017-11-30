a93b7a6c667ab7744792319385dd328bc0ce0153
Dan Danilyuk and Margaret Berry <margaret@>
2017-11-30 09:59:10 -0800

    add crud for images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -15,12 +15,34 @@ class ImagesController < ApplicationController
   def create
     @image = Image.new(image_params)
     if @image.save
-      flash[:notice] = "Post successfully added!"
-      redirect_to root_path
+      flash[:notice] = "Image successfully added!"
+      redirect_to profile_path(current_user.id)
     else
       render :new
     end
   end
+
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    if @image.update(image_params)
+      flash[:notice] = "Image successfully updated!"
+      redirect_to  profile_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    flash[:notice] = "Image successfully deleted!"
+    redirect_to profile_path(current_user.id)
+  end
+
 
 private
   def image_params
