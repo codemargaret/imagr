4e956ab66b121e7c98a73fa8b2ad1e0d04604fad
Dan Danilyuk and Margaret Berry <dan@>
2017-11-30 13:31:11 -0800

    update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,28 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    @image = @comment.image
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Comment successfully updated!"
+      redirect_to  image_path(@comment.image)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Comment successfully deleted!"
+    redirect_to image_path(@comment.image)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body, :image_id, :user_id)
